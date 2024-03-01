d44e8038abe6c9a1193ea00812670537c8f9e7f1
Trevis Suwanwigo <s.taeshawit@gmail.com>
2024-02-29 22:53:14 -0800

    Initial Commit

---
+++ OpenTweet/OpenTweetApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenTweetApp.swift
+//  OpenTweet
+//
+//  Created by Taeshawit Suwanwigo on 2/29/24.
+//
+
+import SwiftUI
+
+@main
+struct OpenTweetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
